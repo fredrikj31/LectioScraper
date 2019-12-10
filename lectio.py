95673f093a0555cb4bd203aa0b1253392c8885e1
fredrikj31 <fredrik1206@gmail.com>
2019-12-10 19:48:40 +0100

    Updated Exercises function. And got the student id

--- lectio.py
+++ lectio.py
@@ -12,7 +12,42 @@ class Lectio:
 		self.Password = Password
 		self.SchoolId = SchoolId
 
-	def login(self):
+		LOGIN_URL = "https://www.lectio.dk/lectio/{}/login.aspx".format(self.SchoolId)
+
+		# Start requests session and get eventvalidation key
+		session = requests.Session()
+		result = session.get(LOGIN_URL)
+		# print(result.text)
+		tree = html.fromstring(result.text)
+		authenticity_token = list(
+			set(tree.xpath("//input[@name='__EVENTVALIDATION']/@value")))[0]
+
+		# Create payload
+		payload = {
+			"m$Content$username2": self.Username,
+			"m$Content$password2": self.Password,
+			"m$Content$passwordHidden": self.Password,
+			"__EVENTVALIDATION": authenticity_token,
+			"__EVENTTARGET": "m$Content$submitbtn2",
+			"__EVENTARGUMENT": "",
+			"LectioPostbackId": ""
+		}
+
+		# Perform login
+		result = session.post(LOGIN_URL, data=payload, headers=dict(referer=LOGIN_URL))
+
+		# Getting student id
+		dashboard = session.get("https://www.lectio.dk/lectio/680/forside.aspx")
+
+		soup = BeautifulSoup(dashboard.text, features="html.parser")
+
+		studentIdFind = soup.find("a", {"id": "s_m_HeaderContent_subnavigator_ctl01"}, href=True)
+
+		self.studentId = (studentIdFind['href']).replace("/lectio/680/forside.aspx?elevid=", '')
+
+		self.Session = session
+
+	"""def login(self):
 		LOGIN_URL = "https://www.lectio.dk/lectio/{}/login.aspx".format(self.SchoolId)
 
 		# Start requests session and get eventvalidation key
@@ -37,9 +72,26 @@ class Lectio:
 		# Perform login
 		result = s.post(LOGIN_URL, data=payload, headers=dict(referer=LOGIN_URL))
 
-		return s
+		return s"""
 
 	def getExercises(self):
-		session = self.login()
+		EXERCISE_URL = "https://www.lectio.dk/lectio/680/OpgaverElev.aspx?elevid={}".format(self.studentId)
 
+		# Scrape url
+		result = self.Session.get(EXERCISE_URL)
+
+		soup = BeautifulSoup(result.text, features="html.parser")
+
+		table = soup.find("table", {"id": "s_m_Content_Content_ExerciseGV"})
+
+		rows = []
+		columns = []
+
+		for row in table.findAll("tr"):
+			#rows.append(row.contents)
+			for row1 in row.findAll('td'):
+				columns.append(row1.text)
+			rows.append(columns)
+			columns = []
 		
+		return rows
