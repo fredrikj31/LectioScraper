4fd5996a00caf27c13c579bf225e1878b02feaba
fredrikj31 <fredrik1206@gmail.com>
2019-12-19 17:25:48 +0100

    Removed unnecessary code

--- lectio.py
+++ lectio.py
@@ -46,33 +46,7 @@ class Lectio:
 		self.studentId = (studentIdFind['href']).replace("/lectio/680/forside.aspx?elevid=", '')
 
 		self.Session = session
-
-	"""def login(self):
-		LOGIN_URL = "https://www.lectio.dk/lectio/{}/login.aspx".format(self.SchoolId)
-
-		# Start requests session and get eventvalidation key
-		s = requests.Session()
-		result = s.get(LOGIN_URL)
-		# print(result.text)
-		tree = html.fromstring(result.text)
-		authenticity_token = list(
-			set(tree.xpath("//input[@name='__EVENTVALIDATION']/@value")))[0]
-
-		# Create payload
-		payload = {
-			"m$Content$username2": self.Username,
-			"m$Content$password2": self.Password,
-			"m$Content$passwordHidden": self.Password,
-			"__EVENTVALIDATION": authenticity_token,
-			"__EVENTTARGET": "m$Content$submitbtn2",
-			"__EVENTARGUMENT": "",
-			"LectioPostbackId": ""
-		}
-
-		# Perform login
-		result = s.post(LOGIN_URL, data=payload, headers=dict(referer=LOGIN_URL))
-
-		return s"""
+		
 
 	def getExercises(self):
 		EXERCISE_URL = "https://www.lectio.dk/lectio/680/OpgaverElev.aspx?elevid={}".format(self.studentId)
