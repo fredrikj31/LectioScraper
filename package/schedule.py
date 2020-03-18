5127be058668298b3e6f738cc36412a2c88d1979
fredrikj31 <fredrik1206@gmail.com>
2020-03-18 16:38:55 +0100

    Added json output and final schedule update

--- package/schedule.py
+++ package/schedule.py
@@ -20,6 +20,7 @@ Schedule["Room"] = row[4]"""
 		"LessonId": "(Lesson Id)",
 		"Status": "(Status)",
 		"Title": "(Title)",
+		"Time": "(Time)",
 		"Team": "(Team)",
 		"Teacher": "(Teacher)",
 		"Room": "(Room)"
@@ -43,21 +44,75 @@ def getSchedule(self, Session, StudentId):
 
 
 	for schedule in scheduleContainer:
-		n = 0
 		rows = schedule['data-additionalinfo'].split("\n")
 		timeStructure = re.compile('\d{2}/\d+-\d{4} \d{2}:\d{2} til \d{2}:\d{2}')
 		teamStructure = re.compile('Hold: ')
 		teacherStructure = re.compile('Lærer.*: ')
 		roomStructure = re.compile('Lokale: ')
+		
+		
+		#Check if there is a status
+		if rows[0] == "Aflyst!" or rows[0] == "Ændret!":
+			#print("found a status: {}".format(rows[0]))
+
+			status = rows[0]
+
+			#Check if there is a title
+			if timeStructure.match(rows[1]):
+				#print("did not find a title")
+				title = " "
+			else:
+				#print("found a title: {}".format(rows[1]))
+				title = rows[1]
+
+		else:
+			#print("did not find any status")
+			status = " "
+
+			#Check if there is a title
+			if timeStructure.match(rows[0]):
+				#print("did not find a title")
+				title = " "
+			else:
+				#print("found a title: {}".format(rows[0]))
+				title = rows[0]
+
 		time = list(filter(timeStructure.match, rows))
 		team = list(filter(teamStructure.match, rows))
 		teacher = list(filter(teacherStructure.match, rows))
 		room = list(filter(roomStructure.match, rows))
-		print(time)
-		print(team)
-		print(teacher)
-		print(room)
-		print("---------------------------")
+
+		#If list is empty (There is no room or teacher) then make list empty
+		if len(time) == 0:
+			time = [' ']
+		if len(team) == 0:
+			team = [' ']
+		if len(teacher) == 0:
+			teacher = [' ']
+		if len(room) == 0:
+			room = [' ']
+
+		Schedule['Status'] = status
+		Schedule['Title'] = title
+		Schedule['Time'] = time[0]
+		Schedule['Team'] = team[0]
+		Schedule['Teacher'] = teacher[0]
+		Schedule['Room'] = room[0]
+		
+
+
+		fullSchedule.append(Schedule)
+		Schedule = {}
+
+		
+		#DEBUG PURPOSES
+		"""
+		print(time[0])
+		print(team[0])
+		print(teacher[0])
+		print(room[0])
+		
+		print("---------------------------")"""
 
 		
 	return fullSchedule
